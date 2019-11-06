bced7e130914cb3cbaf1d5c03467d367b70278b1
Dheeraj Edupuganti <dheeru998@gmail.com>
2019-11-06 16:47:30 -0600

    inserted student data

--- utils/seeder.js
+++ utils/seeder.js
@@ -1,6 +1,8 @@
 const Datastore = require('nedb') // set up a temporary (in memory) database
 const courseData = require('../data/courses.json') // read in data file
 const sectionData = require('../data/section.json') 
+const studentsData=require('../data/students.json') 
+const teachersData=require('../data/teachers.json') 
 
 // inject Express app to configure it - EVERYTHING in through argument list
 
@@ -8,21 +10,36 @@ module.exports = (app) => {
   console.log('START data seeder.')
   const db = {} // empty object to hold all collections
   const sd={}
+  const std={}
+  const td={}
   
   db.courses = new Datastore() // new object property
   sd.sections=new Datastore()
+  std.students=new Datastore()
+  td.teachers=new Datastore()
+
   db.courses.loadDatabase() // call the loadDatabase method
   sd.sections.loadDatabase()
+  std.students.loadDatabase()
+  td.teachers.loadDatabase()
 
   // insert the sample data into our datastore
   db.courses.insert(courseData)
   sd.sections.insert(sectionData)
+  std.students.insert(studentsData)
+  td.teachers.insert(teachersData)
 
   // initialize app.locals (these objects are available to the controllers)
   app.locals.courses = db.courses.find(courseData)
   app.locals.sections=sd.sections.find(sectionData)
+  app.locals.students=std.students.find(studentsData)
+  app.locals.teachers=td.teachers.find(teachersData)
+
+
   console.log(`${app.locals.courses.query.length} courses seeded`)
   console.log(`${app.locals.sections.query.length} section seeded`)
+  console.log(`${app.locals.sections.query.length} student seeded`)
+  console.log(`${app.locals.sections.query.length} Teacher seeded`)
   console.log('END Data Seeder. Sample data read and verified.')
 }
 
