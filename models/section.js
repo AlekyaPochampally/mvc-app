0fec540cbe90df4f4835dd3bd79bbb1fa11a0f82
jyothsna5268 <s536775@nwmissouri.edu>
2019-11-06 16:18:37 -0600

    Section added

---
+++ models/section.js
@@ -0,0 +1,57 @@
+/**
+*  Developer model
+*  Describes the characteristics of each attribute in a developer resource.
+*
+* @author Denise Case <denisecase@gmail.com>
+* @requires mongoose
+*
+*/
+
+const mongoose = require('mongoose')
+
+const courseSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: true
+        
+      },
+      SectionNumber: {
+        type: String,
+        minlength: 2,
+        maxlength: 2,
+        required: true,
+        unique: true
+      },
+      Days: {
+        type: String,
+        minlength: 2,
+        maxlength: 3,
+        required: true,
+        
+        
+      },
+      StartTime: {
+        type: String,
+        minlength: 4,
+        maxlength: 4,
+        required: false,
+       
+      },
+      RoomNumber: {
+        type: String,
+        required: true,
+       
+      },
+      InstructorID: {
+        type: Number,
+        required: true,
+      
+      },
+      CourseID: {
+        type: Number,
+        required: true,
+        
+      }
+    })
+    module.exports = mongoose.model('section', sectionSchema)
+    
--- utils/seeder.js
+++ utils/seeder.js
@@ -1,22 +1,28 @@
 const Datastore = require('nedb') // set up a temporary (in memory) database
 const courseData = require('../data/courses.json') // read in data file
+const sectionData = require('../data/section.json') 
 
 // inject Express app to configure it - EVERYTHING in through argument list
 
 module.exports = (app) => {
   console.log('START data seeder.')
   const db = {} // empty object to hold all collections
-
+  const sd={}
+  
   db.courses = new Datastore() // new object property
+  sd.sections=new Datastore()
   db.courses.loadDatabase() // call the loadDatabase method
+  sd.sections.loadDatabase()
 
   // insert the sample data into our datastore
   db.courses.insert(courseData)
+  sd.sections.insert(sectionData)
 
   // initialize app.locals (these objects are available to the controllers)
   app.locals.courses = db.courses.find(courseData)
+  app.locals.sections=sd.sections.find(sectionData)
   console.log(`${app.locals.courses.query.length} courses seeded`)
-
+  console.log(`${app.locals.sections.query.length} section seeded`)
   console.log('END Data Seeder. Sample data read and verified.')
 }
 
